646ba059244d6ef744e8f2148dbc52096160e341
gyang-pro <6613460+gyang-pro@users.noreply.github.com>
2023-09-04 00:31:58 -0700

    Create functioning grid resize button

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
 const container = document.querySelector('.container');
 
-for(let i = 1; i <= 256; i++) {
-    const square = document.createElement('div');
-    square.classList.add(`square`);
-    square.textContent = i;
-    container.appendChild(square);
+createGrid(16);
+
+const gridButton = document.querySelector('button');
+gridButton.addEventListener('click', () => {
+    let gridNumber = prompt('Type in a number for squares per side for a new grid', '');
+    removeGrid();
+    createGrid(gridNumber);
+});
+
+function createGrid(gridNumber) {
+    while(gridNumber > 100 || gridNumber < 1) {
+        if(gridNumber == null) {
+            return;
+        }
+        gridNumber = prompt('Please type in a number greater than 1 and less than 100', '');
+    }
+
+    for(let i = 1; i <= gridNumber; i++) {
+        for(let j = 1; j <= gridNumber; j++) {
+            const square = document.createElement('div');
+            square.classList.add(`square`);
+            square.style.width = `${1/gridNumber * 100}%`;
+            container.appendChild(square);
+        }
+    }
+
+    draw();
 }
 
-const hover = document.querySelectorAll('.square');
-hover.forEach(square => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'blue';
+function removeGrid() {
+    while(container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function draw() {
+    const hover = document.querySelectorAll('.square');
+    hover.forEach(gridSquare => {
+        gridSquare.addEventListener('mouseover', () => {
+            gridSquare.style.backgroundColor = 'blue';
+        });
     });
-});
+}
